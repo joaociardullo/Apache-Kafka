370ee64e62dc5fb5c93af00dddbfd67481655a14
joaociardullo <joaociardullo@hotmail.com>
2022-09-05 17:25:39 -0300

    add json consumir

---
+++ json-consumer/src/main/java/com/devjoao/jsonconsumer/model/Payment.java
@@ -0,0 +1,18 @@
+package com.devjoao.jsonconsumer.model;
+
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.Serializable;
+
+@ToString
+@Getter
+public class Payment implements Serializable {
+
+    private Long id;
+    private Long idUser;
+    private Long idProduct;
+    private String cardNumber;
+}
+
---
+++ json-consumer/src/test/java/com/devjoao/jsonconsumer/JsonConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjoao.jsonconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
